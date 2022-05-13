c19a728194a56faed95c92639ae10a7e4172ea00
dvasKolbasPC <karelin174@gmail.com>
2022-05-13 13:09:22 +0500

    Добавил 5ю задачу

---
+++ IjuniorTask5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjuniorTask5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Васечкин";
+            string lastName = "Петр";
+
+            Console.WriteLine($"До изменений: Имя: {firstName}, Фамилия: {lastName}");
+
+            string temp = firstName;
+            firstName = lastName;
+            lastName = temp;
+
+            Console.WriteLine($"После изменений:Имя: {firstName}, Фамилия: {lastName}");
+        }
+    }
+}
