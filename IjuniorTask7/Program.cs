d77979362061964c9ad3784166f88108d20a1078
dvasKolbasPC <karelin174@gmail.com>
2022-05-13 13:37:08 +0500

    Добавил 7ю задачу

---
+++ IjuniorTask7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjuniorTask7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = 3;
+            int wallet, numberCrystals;
+
+            Console.WriteLine("Сколько золота в кошельке?");
+            wallet = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Сколько кристаллов купить?");
+            numberCrystals = Convert.ToInt32(Console.ReadLine());
+            wallet -= numberCrystals * price;
+
+            Console.WriteLine($"Золота в кошельке: {wallet}, Кристаллов: {numberCrystals}");
+        }
+    }
+}
