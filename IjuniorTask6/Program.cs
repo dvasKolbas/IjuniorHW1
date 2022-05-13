2119d1189ec69ff5f43c16ed33fe6f71dd17e163
dvasKolbasPC <karelin174@gmail.com>
2022-05-13 13:25:15 +0500

    Добавил 6ю задачу

---
+++ IjuniorTask6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjuniorTask6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictureInRow = 3;
+            int numberPicture = 52;
+
+            Console.WriteLine("Целых рядов: {0}\n" +
+                              "Сверх меры: {1}", numberPicture / pictureInRow, numberPicture % pictureInRow);
+        }
+    }
+}
