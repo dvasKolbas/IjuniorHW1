0467f363d761978be2bd42a5592ad9814cd67896
dvasKolbasPC <karelin174@gmail.com>
2022-05-13 12:39:21 +0500

    Добавил 4ю задачу

---
+++ IjuniorTask4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjuniorTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Сколько вам лет?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Какой у вас знак задиака?");
+            string signZodiac = Console.ReadLine();
+
+            Console.WriteLine("Кем вы работаете?");
+            string work = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {name}, вам {age}лет, ваш знак зодиака {signZodiac}, вы работаете {work}!");
+        }
+    }
+}
