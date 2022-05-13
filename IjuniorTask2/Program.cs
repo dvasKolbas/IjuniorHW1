846b9bf59344b250e8a368a64ac6d8c331898b98
dvasKolbasPC <karelin174@gmail.com>
2022-05-13 12:24:12 +0500

    Добавьте файлы проекта.

---
+++ IjuniorTask2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjuniorHW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue = -1;
+            float secondValue = 2.1f;
+            double thirdValue = 3.512312;
+            uint fouthValue = 1;
+            decimal fifthValue = 0.3m;
+            string myWord = "Hello";
+            char letter = 'a';
+            bool isGenius = true;
+            bool isTrue = false;
+            string myThoughts = "Какие еще нужны переменные?";
+        }
+    }
+}
---
+++ IjuniorTask3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjuniorHW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 38;
+            int c = (31 - 5 * a) / b;
+            Console.WriteLine($"5 * {a} = {5 * a} - первое действие\n" +
+                              $"31 - {5 * a} = {31 - 5 * a} - второе действие\n" +
+                              $"{31 - 5 * a} / {b} = {(31 - 5 * a) / b} - последнее действие, получаем целую часть от деления, int на int");
+        }
+    }
+}
